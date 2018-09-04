1f120ceb086acc1186c3dfe3979ab27a56479490
David Gardiner <david@gardiner.net.au>
2018-09-04 20:23:22 +0930

    Example of NuGet packages with content/contentFiles support

---
+++ NuGet-ContentFiles/ClassLibraryOld/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryOld
+{
+    public class Class1
+    {
+    }
+}
