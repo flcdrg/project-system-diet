39fe078041f15cc48f8a5159e25062e553a2392a
David Gardiner <david@gardiner.net.au>
2018-09-04 20:38:33 +0930

    Copying PDBs from referenced package

---
+++ PDBs-From-Referenced-Packages/ClassLibraryWithPdbs/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibraryWithPdbs
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Creating Class1");
+        }
+    }
+}
---
+++ PDBs-From-Referenced-Packages/ConsoleAppNewNet471/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleAppNewNet471
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cl = new ClassLibraryWithPdbs.Class1();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
