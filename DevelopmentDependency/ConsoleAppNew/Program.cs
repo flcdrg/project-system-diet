f2a1ef16b201b8df8837e36852c1fe349a090fc5
David Gardiner <david@gardiner.net.au>
2018-09-05 17:34:50 +0930

    Finish DevelopmentDependency

---
+++ DevelopmentDependency/ConsoleAppNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
